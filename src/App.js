f72b3d524aa6e6aa5ba375801c5541077b0b0646
narise378 <narise378@gmail.com>
2020-05-21 18:59:29 +0900

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App () {
-  return <div><h1>Hello, World!!</h1></div>;
+class App extends Component {
+ render() {
+  return (
+    //divで囲う必要があるが、不要なdivを排除するためには React.Fragment がでいける
+    <React.Fragment>
+      <label htmlFor="bar">bar </label>
+      <input type="text" onChange={() => {console.log("Click!!")}}/>
+    </React.Fragment>
+  )
+ }
 }
 
 export default App;
