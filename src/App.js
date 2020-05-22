f98be31690dc66244244d76e8fea0c10bce872a4
narise378 <narise378@gmail.com>
2020-05-22 12:41:55 +0900

    learn props or component add comment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  const profiles = [
+  const profiles = [ //配列でオブジェクトを作る
     {name: "Nari", age: 32},
     {name: "Sae", age: 25},
     {name: "Nan"}
@@ -9,18 +9,18 @@ const App = () => {
   return (
   <div>
     {
-      profiles.map((profiles, index) => {
+      profiles.map((profiles, index) => { //引数にprofilesのデータを持たせて、index番号を用いてkeyとして運用しユニークにする
         return <User name={profiles.name} age={profiles.age} key={index}/>
       })
     }
   </div>
   )
 }
-const User = (props) => {
+const User = (props) => { //propsにより親コンポーネントからプロパティを受け取れる
   return <div>Hi, I am {props.name}, and {props.age} years old !</div>
 }
 
-User.defaultProps = {
+User.defaultProps = { //プロパティに値が指定されていないオブジェクトがあればデフォルト値をいれれる
   age: 1
 }
 export default App;
