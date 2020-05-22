2c283dc6f0fc66a92ff9331b1a1314227812455c
narise378 <narise378@gmail.com>
2020-05-22 12:24:19 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
- render() {
+// class App extends Component { クラスコンポーネントはインポートに{ Component }を記述する
+//  render() {
+//   return (
+//     //divで囲う必要があるが、不要なdivを排除するためには React.Fragment がでいける
+//     <React.Fragment>
+//       <label htmlFor="bar">bar </label>
+//       <input type="text" onChange={() => {console.log("Click!!")}}/>
+//     </React.Fragment>
+//   )
+//  }
+// }
+
+const App = () => {
   return (
-    //divで囲う必要があるが、不要なdivを排除するためには React.Fragment がでいける
-    <React.Fragment>
-      <label htmlFor="bar">bar </label>
-      <input type="text" onChange={() => {console.log("Click!!")}}/>
-    </React.Fragment>
+  <div>
+    関数コンポーネント
+    <Cat />
+  </div>
   )
- }
 }
-
+const Cat = () => {
+  return <div>にゃー</div>
+}
 export default App;
